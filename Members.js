40513e16c250984d06b2041b0f944db1f44802fa
joe <joe@joes-Mac-mini.attlocal.net>
2020-02-02 17:14:55 -0600

    init members api

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "john doe",
+    email: "john@john.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "jane deer",
+    email: "jane@jane.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    name: "jean luke",
+    email: "jean@jean.com",
+    status: "inactive"
+  }
+]
+
+module.exports = members
---
+++ README.md
@@ -0,0 +1,19 @@
+# Express api
+
+Basic api for handling members.
+
+## Requirements
+
+node & npm
+
+## Install
+
+```
+git clone git@github.com:joekennerly/express-api.git
+cd express-api
+npm install
+```
+
+## Run the app
+
+`npm run dev`
